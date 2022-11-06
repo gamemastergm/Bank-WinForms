6fe52dd35c7f881141eafa9e0410d26ef86aab7f
GameMaster <felipe.diniz@ucsal.edu.br>
2022-11-06 19:48:39 -0300

    add bank

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace Bank
 {
@@ -14,9 +16,27 @@ namespace Bank
         [STAThread]
         static void Main()
         {
+            TestConnection();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            --Console.ReadKey();
+
+        }
+        private static void TestConnection()
+        {
+            using(NpgsqlConnection con = GetConnection()) 
+            { 
+                con.Open();
+                if (con.State == System.Data.ConnectionState.Open)
+                {
+                    Debug.WriteLine("Connected");
+                }
+            }
+        }
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;port=5432;user Id=postgres;Password=postgres;Database=Bank");
         }
     }
 }
